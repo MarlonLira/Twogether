34c39d9135f347fa9f90d8a2dfa97af7b206b1c0
Marlon Lira <marlonlira3@gmail.com>
2019-05-12 20:05:36 -0300

    updates

--- Twogether/App_Start/BundleConfig.cs
+++ Twogether/App_Start/BundleConfig.cs
@@ -10,11 +10,11 @@ namespace Twogether {
         // Para obter mais informações sobre o Agrupamento, visite https://go.microsoft.com/fwlink/?LinkID=303951
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/Js").Include(
-                            "~/Scripts/jquery-3.3.1.min.js",
+                            "~/Scripts/vendor/jquery-1.12.4.min.js",
                             "~/Scripts/bootstrap.min.js",
-                            "~/Scripts/moment.min.js",
+                            "~/Scripts/wow.min.js",
                             "~/Scripts/Chart.min.js",
-                            "~/Scripts/tooplate-scripts.js"
+                            "~/Scripts/moment.min.js"
                             ).IncludeDirectory("~/Scripts", ".js"));
 
             bundles.Add(new StyleBundle("~/bundles/Css").Include(
@@ -23,11 +23,6 @@ namespace Twogether {
                             "~/Content/style.css",
                             "~/Content/responsive.css"
                             ).IncludeDirectory("~/Content", ".css"));
-
-            // Use a versão de Desenvolvimento do Modernizr para se desenvolver e aprender com ele. Em seguida, quando estiver
-            // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                            "~/Scripts/modernizr-*"));
         }
     }
 }
